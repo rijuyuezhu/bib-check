8a124523dba0e6dec89a0f32d82a685ef4cc6852
Wenrui Huang <rijuyuezhuhwr@gmail.com>
2025-05-09 17:15:07 +0800

    feat(dblp): add dblp search functionality

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 Automatically convert bibtex to a good-form one.
 
+## Usage
+
 ```
-usage: bib-check.py [-h] [--ai] [--ai-service AI_SERVICE] [--ai-model AI_MODEL]
-                    [--ai-key AI_KEY]
-                    input_bib_file [output_bib_file]
+usage: bib-check.py [-h] [--ai] [--ai-service AI_SERVICE] [--ai-model AI_MODEL] [--ai-key AI_KEY] [--dblp] input_bib_file [output_bib_file]
 
 positional arguments:
   input_bib_file        Path to the input bib file
@@ -18,12 +18,29 @@ options:
                         AI service name (default: deepseek)
   --ai-model AI_MODEL   AI model name (default: deepseek-chat)
   --ai-key AI_KEY       API key for the AI service
+  --dblp                Convert the entry to one on DBLP
 ```
 
-## Usage
-
-I recommend use `uv`:
+I recommend use `uv` to run the script, which reads the dependencies from the script itself
 
 ```bash
-uv run --script bib-check.py <other-args>
+uv run bib-check.py <other-args>
 ```
+
+## Workflow
+
+I recommend using two phases to run the script.
+
+First check the dblp, and fix the issues if any.
+
+```bash
+uv run bib-check.py 1.bib 2.bib --dblp
+```
+
+Then use ai to convert the titles/entries
+
+```bash
+uv run bib-check.py 2.bib 3.bib --ai --ai-service <service> --ai-model <model> --ai-key <key>
+```
+
+After that, diff `2.bib` and `3.bib` to ensure correctness.
--- bib-check.py
+++ bib-check.py
@@ -5,17 +5,17 @@
 #     "bibtexparser==2.0.0b8",
 #     "logging",
 #     "openai",
-#     "setuptools",
+#     "requests",
 # ]
 # ///
-
-
 import argparse
 import bibtexparser
 import logging
+import requests
 
 from openai import OpenAI
 from bibtexparser.model import Entry
+from urllib.parse import urlencode
 
 logger = logging.getLogger("bib-check")
 
@@ -42,7 +42,38 @@ def parse_args() -> argparse.Namespace:
         help="AI model name (default: deepseek-chat)",
     )
     parser.add_argument("--ai-key", type=str, help="API key for the AI service")
+    parser.add_argument(
+        "--dblp", action="store_true", help="Convert the entry to one on DBLP"
+    )
     return parser.parse_args()
+
+
+class DblpSearch:
+    # The following code in this class is adaptive from
+    # https://github.com/alumik/dblp-api/
+    #
+    # MIT License
+    # Copyright (c) 2021,2023 Zhong Zhenyu
+    BASE_URL = "https://dblp.org/search/publ/api"
+
+    @staticmethod
+    def search(query: str) -> list[dict]:
+        results = []
+        options = {"q": query, "format": "json", "h": 100}
+        r = requests.get(f"{DblpSearch.BASE_URL}?{urlencode(options)}").json()
+        hits = r.get("result").get("hits").get("hit")
+        if hits is not None:
+            for hit in hits:
+                info = hit.get("info")
+                entry = {}
+                entry["title"] = info.get("title")
+                entry["year"] = info.get("year")
+                entry["venue"] = info.get("venue")
+                entry["doi"] = info.get("doi")
+                entry["url"] = info.get("ee")
+                entry["bibtex"] = f"{info.get('url')}" + ".bib"
+                results.append(entry)
+        return results
 
 
 class BibConverter:
@@ -52,6 +83,7 @@ class BibConverter:
         ai_service: str | None = None,
         ai_key: str | None = None,
         ai_model: str | None = None,
+        use_dblp: bool = False,
     ):
         self.use_ai = use_ai
         self.ai_client = None
@@ -62,6 +94,44 @@ class BibConverter:
                 api_key=ai_key, base_url=f"https://api.{ai_service}.com/v1/"
             )
             self.ai_model = ai_model
+        self.use_dblp = use_dblp
+
+    def check_dblp(self, entry: Entry) -> None:
+        def replace_entry(hit: dict) -> None:
+            bibcontent = requests.get(hit["bibtex"]).text
+            lib = bibtexparser.parse_string(bibcontent)
+            if len(lib.entries) != 1:
+                logger.warning(
+                    "Failed to parse bibtex from DBLP @ line %d", entry.start_line
+                )
+                return
+            new_entry = lib.entries[0]
+            entry.fields = new_entry.fields
+
+        if "title" not in entry:
+            logger.warning("Missing title in entry @ line %d", entry.start_line)
+            return
+        hits = DblpSearch.search(entry["title"])
+        if len(hits) == 0:
+            logger.warning("No hits for `%s` in DBLP @ line %d", entry.start_line)
+        elif len(hits) == 1:
+            replace_entry(hits[0])
+        else:
+            # use a CLI to choose
+            print(
+                f"Multiple hits for \033[1;31m{entry['title']}\033[0m in DBLP, please check manually"
+            )
+            while True:
+                for i, hit in enumerate(hits):
+                    print(f"{i}: {hit['title']}, {hit['year']}, {hit['venue']}")
+                choice = input(
+                    "Please choose the correct one (0-%d): " % (len(hits) - 1)
+                )
+                if choice.isdigit() and 0 <= int(choice) < len(hits):
+                    replace_entry(hits[int(choice)])
+                    break
+                else:
+                    print("Invalid choice, input again")
 
     def ai_help(self, old_name: str, system_prompt: str) -> str:
         if not self.ai_client:
@@ -86,8 +156,7 @@ class BibConverter:
             return old_name
 
     def ai_help_title(self, old_title: str) -> str:
-        system_prompt = \
-"""
+        system_prompt = """
 You are given a title of bibtex entries, and try to fix it.
 The requirement is that the conference/journal name:
 1. The title is shown of the sentence form, no matter the original lower/upper form.
@@ -102,8 +171,7 @@ Some examples are
         return self.ai_help(old_title, system_prompt)
 
     def ai_help_journal_or_booktitle(self, old_name: str) -> str:
-        system_prompt = \
-"""
+        system_prompt = """
 You are given an conference/journal name of bibtex entries, and try to fix it.
 The requirement is that the conference/journal name:
 1. Change some letters from lower case to upper case, according to the convention of the conference/journal name.
@@ -123,8 +191,8 @@ Proceedings of the 29th Symposium on Operating Systems Principles
         fields = []
         for key in required:
             if key not in entry:
-                logger.warning("Missing %s in entry @ %d", key, entry.start_line)
-                return
+                logger.warning("Missing `%s` in entry @ line %d", key, entry.start_line)
+                continue
             field = entry.fields_dict[key]
             if self.use_ai:
                 if key == "title":
@@ -139,8 +207,8 @@ Proceedings of the 29th Symposium on Operating Systems Principles
         fields = []
         for key in required:
             if key not in entry:
-                logger.warning("Missing %s in entry %d", key, entry.start_line)
-                return
+                logger.warning("Missing `%s` in entry @ line %d", key, entry.start_line)
+                continue
             field = entry.fields_dict[key]
             if self.use_ai:
                 if key == "title":
@@ -157,12 +225,16 @@ Proceedings of the 29th Symposium on Operating Systems Principles
         )
         for entry in library.entries:
             if entry.entry_type == "article":
+                if self.use_dblp:
+                    self.check_dblp(entry)
                 self.convert_article(entry)
             elif entry.entry_type == "inproceedings":
+                if self.use_dblp:
+                    self.check_dblp(entry)
                 self.convert_inproceedings(entry)
             else:
                 logger.warning(
-                    "Manually check bibentry @ %d of type %s",
+                    "Manually check bibentry @ line %d of type %s",
                     entry.start_line,
                     entry.entry_type,
                 )
@@ -170,20 +242,38 @@ Proceedings of the 29th Symposium on Operating Systems Principles
 
 
 def main() -> None:
-    logging.basicConfig(level=logging.WARNING)
+    logging.basicConfig(
+        level=logging.WARNING,
+        filename="bib-check.log",
+        filemode="w",
+        format="%(message)s",
+    )
     args = parse_args()
     input_bib_file: str = args.input_bib_file
     output_bib_file: str | None = args.output_bib_file
     if output_bib_file is None:
         if input_bib_file.endswith(".bib"):
-            output_bib_file = input_bib_file.replace(".bib", "_formatted.bib")
+            output_bib_file = input_bib_file.replace(".bib", ".checked.bib")
         else:
-            output_bib_file = input_bib_file + "_formatted.bib"
+            output_bib_file = input_bib_file + ".checked.bib"
 
-    converter = BibConverter(args.ai, args.ai_service, args.ai_key, args.ai_model)
+    converter = BibConverter(
+        use_ai=args.ai,
+        ai_service=args.ai_service,
+        ai_key=args.ai_key,
+        ai_model=args.ai_model,
+        use_dblp=args.dblp,
+    )
     with open(input_bib_file, "r") as f:
         bib_data = f.read()
     converter.bib_convert(bib_data, output_bib_file)
+    # print the log file
+    with open("bib-check.log", "r") as f:
+        log_data = f.read()
+    print(
+        "\n\033[1;31mThe main process finished. Please check the following issues\033[0;m\n"
+    )
+    print(log_data)
 
 
 if __name__ == "__main__":
